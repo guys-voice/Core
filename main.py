d7dd1fc3c2b2e18196ea2bc51c20345b75e536b3
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-05 19:09:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -120,34 +120,6 @@ def send_inline_query_results(inline_query_id, results, offset):
     }
     requests.post(url, json=data, headers=headers)
 
-# used to show the voice choices for the authorised user only once the program starts
-
-def send_show_options_to_users():
-    for user_id in AUTHORIZED_USER_IDS:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-        # Get all values from voices[1]
-        options = [voice[1] for voice in voices]
-
-        keyboard = {
-            'keyboard': [[option] for option in options],
-            'one_time_keyboard': True,
-            'resize_keyboard': True
-        }
-
-        text = "Choose a voice title:"
-        data = {
-            'chat_id': user_id,
-            'text': text,
-            'reply_markup': json.dumps(keyboard)
-        }
-
-        response = requests.post(url, data=data)
-        if response.status_code != 200:
-            print(f"Failed to send options to user {user_id}: {response.content}")
-        else:
-            print(f"Successfully sent to user {user_id}")
-
 def send_actual_voices_to_dm(update):
     user_id = update['message']['from']['id']
     user_text = update['message']['text']
@@ -206,9 +178,6 @@ def main():
 
 # used to start the program
 if __name__ == "__main__":
-    # Uncomment the following line to send options to users
-    # send_show_options_to_users()
-    # run the bot
     last_update_id = -1
     print('Bot starts working...')
     main()
