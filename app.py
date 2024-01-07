866f3f21c1df0e9b3065f88fdf0fa98d2ab6ef09
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 16:02:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,43 +6,42 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Flask Application!"
 
-@app.route('/neon', methods=['GET'])
-def admin():
-    return "Hello Komiljon! Here you can use /menu | /stats | /users"
-
-@app.route('/run<int:run_number>', methods=['GET', 'POST', 'HEAD'])
-def run_command(run_number):
-    if request.method in ['GET', 'POST', 'HEAD']:
-        command_result = execute_command(f'python3 main.py for run{run_number}')
-        return command_result
-
-def execute_command(command):
-    import subprocess
-    try:
-        result = subprocess.run(command.split(), capture_output=True, text=True)
-        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
-    except Exception as e:
-        return f"Error: {str(e)}\n"
-
-@app.route('/menu', methods=['GET'])
-def menu():
+#@app.route('/neon', methods=['GET'])
+#def admin():
+#    return "Hello Komiljon! Here you can use /menu | /stats | /users"
+#
+#@app.route('/run<int:run_number>', methods=['GET', 'POST', 'HEAD'])
+#def run_command(run_number):
+#    if request.method in ['GET', 'POST', 'HEAD']:
+#        command_result = execute_command(f'python3 main.py for run{run_number}')
+#        return command_result
+#
+#def execute_command(command):
+#    import subprocess
+#    try:
+#        result = subprocess.run(command.split(), capture_output=True, text=True)
+#        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
+#    except Exception as e:
+#        return f"Error: {str(e)}\n"
+#
+@app.route('/core', methods=['GET'])
+def core():
     import subprocess
     if request.method == 'GET':
-        result = subprocess.run('timeout 29 python3 main.py', capture_output=True, text=True)
-        subprocess.run('timeout 1 curl -X GET https://neon-fkki.onrender.com/menu', capture_output=True, text=True)
+        result = subprocess.run('timeout 1 python3 run_infinetely.py', capture_output=True, text=True)
         return "The command is executed! Results: {result}"
 
-@app.route('/stats', methods=['GET'])
-def stats():
-    if request.method == 'GET':
-        result = subprocess.run('python3 stats.py', capture_output=True, text=True)
-        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
-
-@app.route('/users', methods=['GET'])
-def users():
-    if request.method == 'GET':
-        result = subprocess.run('python3 users.py', capture_output=True, text=True)
-        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
+#@app.route('/stats', methods=['GET'])
+#def stats():
+#    if request.method == 'GET':
+#        result = subprocess.run('python3 stats.py', capture_output=True, text=True)
+#        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
+#
+#@app.route('/users', methods=['GET'])
+#def users():
+#    if request.method == 'GET':
+#        result = subprocess.run('python3 users.py', capture_output=True, text=True)
+#        return f"Command Output: {result.stdout}\nCommand Error: {result.stderr}\n"
 
 if __name__ == '__main__':
     app.run(debug=True)
