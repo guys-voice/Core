f1d3d99e23418a78813c1083232529ad426dc9ef
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 10:29:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 # here are the global variables
 
-from variables import BOT_TOKEN, ADMIN, GROUP, AUTHORIZED_USER_IDS, voices, last_update_id, last_sent_time
+from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, voices, last_update_id, last_sent_time
 
 # used to handle inline requests and send results if the user is authorised and ignore and pass in to upcoming function otherwise
 def inline_query(update):
