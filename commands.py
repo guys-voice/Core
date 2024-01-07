0b3810e9f2bc2131cd3c0460b12d5a957b048b57
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 14:05:24 +0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -4,42 +4,53 @@ from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VO
 def commands(user_id, name, message):
     if message == '/start':
         if not any(user_id in line for line in open('users.txt')):
-        with open('users.txt', 'a') as file:
-            file.write(f"{user_id} {name}\n")
+            with open('users.txt', 'a') as file:
+                file.write(f"{user_id} {name}\n")
+        data = {
+            'chat_id': user_id,
+            'text': f"Welcome {name}. Happy to see here.",
+            'parse_mode': 'Markdown'
+        }
+        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+            
     elif message == '/show':
-        return
-    elif message == '/users':
-        return
-    elif message == '/stats':
-        return
-    elif message == '/neon':
-        return
-    else:
-        return
-
-def send_show_options_to_users(user_id):
-    for user_id in AUTHORIZED_USER_IDS:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-        # Get all values from voices[1]
-        options = [voice[1] for voice in voices]
-
+        options = [voice[1] for voice in VOICES]
         keyboard = {
             'keyboard': [[option] for option in options],
             'one_time_keyboard': True,
             'resize_keyboard': True
         }
-
-        text = "Choose a voice title:"
         data = {
             'chat_id': user_id,
-            'text': text,
+            'text': "Choose a voice title. I will send the voice message.",
             'reply_markup': json.dumps(keyboard)
         }
-
-        response = requests.post(url, data=data)
-        if response.status_code != 200:
-            print(f"Failed to send options to user {user_id}: {response.content}")
-        else:
-            print(f"Successfully sent to user {user_id}")
+        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+        
+    elif message == '/users':
+        with open('users.txt', 'r') as file:
+            lines = file.readlines()
+            number_of_users = len(lines)
+            data = {
+                'chat_id': user_id,
+                'text': f"Currently, we have {number_of_users} users.",
+                'parse_mode': 'Markdown'
+            }
+            requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+    elif message == '/stats':
+        data = {
+            'chat_id': user_id,
+            'text': f"We still working on this...",
+            'parse_mode': 'Markdown'
+        }
+        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+    elif message == '/neon':
+        data = {
+            'chat_id': user_id,
+            'text': f"Hello {name}, you have successfully logged in as a root user!",
+            'parse_mode': 'Markdown'
+        }
+        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+    else:
+        return
 # stats, users, neon kabi functionla shu yerda bo'ladi bulani ham webdan ham botdan turib ishlatsa bo'ladi preferable webdan faqat keep alivega ishlataman va qolganini sirli commandla bilan qilaman.
