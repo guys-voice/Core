29a157af956d82bc0ae5994b432f94201733e601
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 22:04:00 +0500

    Update special.py

--- special.py
+++ special.py
@@ -2,8 +2,20 @@ import requests
 
 def special(message):
   if message[:12] == 'SEND_MESSAGE':
-    message[14:23]
+    data = {
+      'chat_id': int(message[14:23]),
+      'text': message[25:],
+      'parse_mode': 'Markdown'
+    }
+    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+    return
   elif message[:10] == 'SEND_VOICE':
-    message[10:21]
+    data = {
+      'chat_id': int(message[10:21]),
+      'text': message[23:],
+      'parse_mode': 'Markdown'
+    }
+    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+    return
   else:
     return
