52ed5ce7c953f1709b9894eaeb0759c878372311
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 15:57:36 +0500

    Update run_infinetely.py

--- run_infinetely.py
+++ run_infinetely.py
@@ -1,1 +1,15 @@
-
+data1 = {
+  'chat_id': ADMIN,
+  'text': "I will be running, until I say I have finished.",
+  'parse_mode': 'Markdown'
+}
+requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+try:
+  subprocess.run(['python3', 'main.py'])
+except Exception as e:
+  data = {
+    'chat_id': ADMIN,
+    'text': f"I have finished. Error executing command: {e}",
+    'parse_mode': 'Markdown'
+  }
+  requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
