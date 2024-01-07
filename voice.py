4ae43efb9e326353eb536c44b5fd039069633206
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 11:58:21 +0500

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,5 +1,5 @@
 import requests
-from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, voices, last_update_id, last_sent_time
+from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, last_update_id, last_sent_time
 
 def message_unauth(user_id):
     text = (f"Hmm, I understand that you are very curious, but I can not share anything with you right now! Try "
