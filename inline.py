803a2e2831e50b2d757734adaa43edeab64d169b
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 13:01:53 +0500

    Update inline.py

--- inline.py
+++ inline.py
@@ -3,9 +3,8 @@ import requests
 from uuid import uuid4
 from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, last_update_id, last_sent_time
 
-def inline_query(update):
+def inline_unauth(update):
     user_id = update['inline_query']['from']['id']
-    off = update['inline_query']['offset']
     if user_id not in AUTHORIZED_USER_IDS:
         unauthorized_message = "*Contact* ➡️ @boot\_to\_root"
         results = [
@@ -25,20 +24,19 @@ def inline_query(update):
         }
         requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/answerInlineQuery", data=data)
         return
-
+        
+def inline_auth(update)
+    off = update['inline_query']['offset']
+    user_id = update['inline_query']['from']['id']
     query = update['inline_query']['query'].lower()
     filtered_voices = [(url, title) for url, title in VOICES if query in title.lower()]
-
     offset = int(off) if off and off != 'null' else 0
     next_offset = str(offset + 20) if offset + 20 < len(VOICES) else ''
-
     results = []
-
     if user_id == ADMIN:
         caption = '෴'
     else:
         caption = ''
-
     for voice_url, title in filtered_voices[offset:offset+20]:
         results.append({
             'id': str(uuid4()),
@@ -48,16 +46,7 @@ def inline_query(update):
             'caption': caption #(caption function doesnot work as expected)
         })
     send_inline_query_results(update['inline_query']['id'], results, next_offset)
-
-# used to actually send the inline text to unauthorised user
-def send_inline_query_message(inline_query_id, results):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/answerInlineQuery"
-    data = {
-        'inline_query_id': inline_query_id,
-        'results': json.dumps(results)
-    }
-    requests.post(url, data=data)
-
+    
 def send_inline_query_results(inline_query_id, results, offset):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/answerInlineQuery"
     data = {
