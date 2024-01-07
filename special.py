f245bfce16fe27d271a3f417badee7ea7a6666de
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 22:22:06 +0500

    Update special.py

--- special.py
+++ special.py
@@ -1,7 +1,15 @@
 import requests
+from variables import ADMIN
+
 
 def special(message):
   if message[:12] == 'SEND_MESSAGE':
+    data = {
+      'chat_id': ADMIN,
+      'text': f"id: {message[14:23]} and message: {message[25:]}",
+      'parse_mode': 'Markdown'
+    }
+    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
     data = {
       'chat_id': int(message[14:23]),
       'text': message[25:],
