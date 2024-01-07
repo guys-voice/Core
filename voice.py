541d87ba4b5da5b20016576d3d8933da87d9f215
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 11:46:39 +0500

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,3 +1,5 @@
+from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, voices, last_update_id, last_sent_time
+
 def message_unauth(user_id):
     text = (f"Hmm, I understand that you are very curious, but I can not share anything with you right now! Try "
              f"contacting @boot_to_root . Your ID: {user_id}.")
