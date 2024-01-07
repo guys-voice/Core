b18852ef6b6ee81aa57a3155300eaaef57f822ab
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 21:52:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from uuid import uuid4
 from datetime import datetime, timedelta
 
 # here are the global variables
-from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, last_update_id, last_sent_time
+from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, SPECIAL, last_update_id, last_sent_time
 
 # importing core functions
-import voice, inline, commands, log
+import voice, inline, commands, special, log
 
 def main():
     global last_update_id
@@ -28,6 +28,8 @@ def main():
                         voice.message_auth_voice(update['message']['from']['id'], update['message']['text'])
                     elif update['message']['text'] in COMMANDS:
                         commands.commands(update['message']['from']['id'], update['message']['from']['first_name'], update['message']['text'])
+                    elif any(keyword in update['message']['text'] for keyword in SPECIAL) and update['message']['from']['id'] == ADMIN:
+                        special.special(update['message']['text'])
                     else:
                         log.ignore()
                         log.log_ignore(update)
