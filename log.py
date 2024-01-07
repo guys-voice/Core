dcbf52f1479f5977b8549edf0e0918b4f2b1c087
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-07 11:57:26 +0500

    Update log.py

--- log.py
+++ log.py
@@ -1,4 +1,4 @@
-from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, voices, last_update_id, last_sent_time
+from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, last_update_id, last_sent_time
 
 def log_unauth(update):
     return update
