b5440cc849bdab5c9c8236f76ad5a2e4ac5145ab
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-01-08 17:03:03 +0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -17,17 +17,23 @@ def commands(user_id, name, message):
             
     elif message == '/show':
         options = [voice[1] for voice in VOICES]
-        keyboard = {
-            'keyboard': [[option] for option in options],
-            'one_time_keyboard': True,
-            'resize_keyboard': True
-        }
+        chunks = [options[i:i + 20] for i in range(0, len(options), 20)]
+        inline_keyboard = [
+            [{"text": title, "callback_data": f"voice_option:{title}"}] for title in chunks[0]
+        ]
+        inline_keyboard.append([{"text": "Next", "callback_data": f"next_page:1"}])
+        reply_markup = {"inline_keyboard": inline_keyboard}
+        text = "Choose a voice title:"
         data = {
             'chat_id': user_id,
-            'text': "Choose a voice title. I will send the voice message.",
-            'reply_markup': json.dumps(keyboard)
+            'text': text,
+            'reply_markup': json.dumps(reply_markup)
         }
-        requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+        response = requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data=data)
+        if response.status_code != 200:
+            print(f"Failed to send options to user {user_id}: {response.content}")
+        else:
+            print(f"Successfully sent to user {user_id}")
         
     elif message == '/users': # not working still
         with open('users.txt', 'r') as file:
