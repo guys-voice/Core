2b85c7ed226f21c4414de0719c14211c17f5a456
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-02-15 23:58:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['POST'])
 def handle_webhook():
     try:
@@ -14,10 +15,12 @@ def handle_webhook():
     except Exception as e:
         return e
 
+
 from variables import BOT_TOKEN, ADMIN, GROUP, COMMANDS, AUTHORIZED_USER_IDS, VOICES, SPECIAL, last_sent_time
 
 # importing core functions
 import voice, inline, commands, special, log
+
 
 def process(update):
     if 'inline_query' in update:
@@ -30,20 +33,26 @@ def process(update):
     elif 'message' in update and 'text' in update['message']:
         if update['message']['from']['id'] in AUTHORIZED_USER_IDS:
             log.log_auth(update)
-            if any(update['message']['text'] == voice[1] for voice in VOICES): #not efficient though
+            if any(update['message']['text'] == voice[1] for voice in VOICES):  # not efficient though
                 voice.message_auth_voice(update['message']['from']['id'], update['message']['text'])
             elif update['message']['text'] in COMMANDS:
-                commands.commands(update['message']['from']['id'], update['message']['from']['first_name'], update['message']['text'])
-            elif update['message']['text'][:12] == 'SEND_MESSAGE': #any(keyword in update['message']['text'][:12] for keyword in SPECIAL) and update['message']['from']['id'] == ADMIN:
+                commands.commands(update['message']['from']['id'], update['message']['from']['first_name'],
+                                  update['message']['text'])
+            elif update['message']['text'][
+                 :12] == 'SEND_MESSAGE':  # any(keyword in update['message']['text'][:12] for keyword in SPECIAL) and update['message']['from']['id'] == ADMIN:
                 special.special(update['message']['text'])
             elif update['message']['text'] == '/Hammasi':
-				for i in range(len(VOICES)):
-					data = {
-						'chat_id': update['message']['from']['id'],
-						'voice': VOICES[i][0],
-					}
-					reply_id = requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendVoice", json=data).json()['result']['message_id']
-					print(requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage',json={'chat_id': update['message']['from']['id'],'text': f"{VOICES[i][1]}",'reply_to_message_id': reply_id}).json())
+                for i in range(len(VOICES)):
+                    data = {
+                        'chat_id': update['message']['from']['id'],
+                        'voice': VOICES[i][0],
+                    }
+                    reply_id = \
+                    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendVoice", json=data).json()['result'][
+                        'message_id']
+                    print(requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage',
+                                        json={'chat_id': update['message']['from']['id'], 'text': f"{VOICES[i][1]}",
+                                              'reply_to_message_id': reply_id}).json())
             else:
                 log.ignore()
                 log.log_ignore(update)
