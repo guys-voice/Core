fc94aa2937883b6313ad1cd9284db0ef5049d9fe
guys-voice <152061721+guys-voice@users.noreply.github.com>
2024-02-16 00:24:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def process(update):
             elif update['message']['text'] == '/Hammasi':
                 count = 1
                 for i in range(len(VOICES)):
-                    if count <= 97:
+                    if count <= 149:
                         count = count + 1
                         continue
                     data = {
